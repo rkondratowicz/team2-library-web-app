52d88638a9fc352e3380a5e246fe1a8805be9ebc
ryanmagee-kainos <ryan.magee@kainos.com>
2025-09-23 11:47:29 +0100

    deleting modules - before adding gitignore (#9)

--- app.ts
+++ app.ts
@@ -13,7 +13,8 @@ app.get('/api/books', (req, res) => {
   const books = [
     { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
     { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee' },
-    { id: 3, title: '1984', author: 'George Orwell' }
+    { id: 3, title: '1984', author: 'George Orwell' },
+    { id: 4, title: 'Harry Potter', author: 'J.K. Rowling' }
   ];
   res.json(books);
 });
